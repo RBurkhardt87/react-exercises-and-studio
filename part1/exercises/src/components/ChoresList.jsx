e088faadd79583fd17ac554e1684b404e6a42942
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-08-21 13:29:57 -0500

    Working on the movies and chores list

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,20 @@
+
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <>
+      <h3 className= {classes.choresText}>Chores List</h3>
+      
+      <ol className={classes.choresHeading}>
+         <li>Get Miles ready for school</li>
+         <li>Get laundry washed/folded</li>
+         <li>Meal prep for the week</li>
+         <li>Do some homework done</li>   
+      </ol>  
+      
+      </>
+   )
+   
+      
 }
